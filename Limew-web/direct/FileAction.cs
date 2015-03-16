a0ca3cab07e97350511bea7a0d40e9c931a87f85
canred.chen@code.google.com <canred.chen@code.google.com>
2015-03-16 10:06:48 +0800

    cr

---
+++ Limew-web/direct/FileAction.cs
@@ -0,0 +1,229 @@
+﻿#region USING
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using ExtDirect;
+using ExtDirect.Direct;
+using LK.DB.SQLCreater;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+using Limew.Model.Basic;
+using Limew.Model.Basic.Table;
+using Limew.Model.Basic.Table.Record;
+using Limew;
+using Limew.Model.Lw;
+using Limew.Model.Lw.Table;
+using Limew.Model.Lw.Table.Record;
+using System.Text;
+using LK.Util;
+using System.Data;
+using System.Diagnostics;
+#endregion
+
+[DirectService("FileAction")]
+public class FileAction : BaseAction
+{
+
+    [DirectMethod("infoFile", DirectAction.Load, MethodVisibility.Visible)]
+    public JObject infoFile(string pFileUuid, Request request)
+    {
+        #region Declare
+        LwModel model = new LwModel();
+        #endregion
+
+        try
+        {  /*Cloud身份檢查*/
+            checkUser(request.HttpRequest);
+            if (this.getUser() == null)
+            {
+                throw new Exception("Identity authentication failed.");
+            }/*權限檢查*/
+            if (!checkProxy(new StackTrace().GetFrame(0)))
+            {
+                throw new Exception("Permission Denied!");
+            };
+            var data = model.getFile_By_FileUuid(pFileUuid);
+
+            if (data.AllRecord().Count == 1)
+            {
+                return ExtDirect.Direct.Helper.Form.OutputJObject(JsonHelper.RecordBaseJObject(data.AllRecord().First()));
+            }
+            return ExtDirect.Direct.Helper.Message.Fail.OutputJObject(new Exception("Data Not Found!"));
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex); LK.MyException.MyException.Error(this, ex);
+            return ExtDirect.Direct.Helper.Message.Fail.OutputJObject(ex);
+        }
+    }
+
+    [DirectMethod("loadVFilegroup", DirectAction.Store, MethodVisibility.Visible)]
+    public JObject loadVFilegroup(string pFilegroupUuid,string pKeyword, string pageNo, string limitNo, string sort, string dir, Request request)
+    {
+        #region Declare
+        List<JObject> jobject = new List<JObject>();
+        Limew.Model.Basic.BasicModel model = new Limew.Model.Basic.BasicModel();
+        LwModel mod = new LwModel();
+        OrderLimit orderLimit = new OrderLimit();
+
+        #endregion
+        try
+        {     /*Cloud身份檢查*/
+            checkUser(request.HttpRequest);
+            if (this.getUser() == null)
+            {
+                throw new Exception("Identity authentication failed.");
+            }/*權限檢查*/
+            if (!checkProxy(new StackTrace().GetFrame(0)))
+            {
+                throw new Exception("Permission Denied!");
+            };
+            /*取得總資料數*/
+            orderLimit = ExtDirect.Direct.Helper.Order.getOrderLimit(pageNo, limitNo, sort, dir);
+            var totalCount = mod.getVFilegroup_By_Keyword_Count(pFilegroupUuid,pKeyword);
+            var data = mod.getVFilegroup_By_Keyword(pFilegroupUuid,pKeyword, orderLimit);
+            if (data.Count > 0)
+            {
+                jobject = JsonHelper.RecordBaseListJObject(data.ToList());
+            }
+            else
+            {
+                totalCount = 0;
+            }
+            /*使用Store Std out 『Sotre物件標準輸出格式』*/
+            return ExtDirect.Direct.Helper.Store.OutputJObject(jobject, totalCount);
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex); LK.MyException.MyException.Error(this, ex);
+            /*將Exception轉成EXT Exception JSON格式*/
+            return ExtDirect.Direct.Helper.Message.Fail.OutputJObject(ex);
+        }
+    }
+
+    [DirectMethod("submitFile", DirectAction.FormSubmission, MethodVisibility.Visible)]
+    public JObject submitFile(string file_uuid,
+string file_name,
+string file_ps,
+string filegroup_uuid, HttpRequest request)
+    {
+
+
+        #region Declare
+        var action = SubmitAction.None;
+        LwModel mod = new LwModel();
+        File_Record record = new File_Record();
+        #endregion
+        try
+        {  /*Cloud身份檢查*/
+            checkUser(request);
+            if (this.getUser() == null)
+            {
+                throw new Exception("Identity authentication failed.");
+            }/*權限檢查*/
+            if (!checkProxy(new StackTrace().GetFrame(0)))
+            {
+                throw new Exception("Permission Denied!");
+            };
+            /*
+             * 所有Form的動作最終是使用Submit的方式將資料傳出；
+             * 必須有一個特徵來判斷使用者，執行的動作；
+             */
+            if (file_uuid.Trim().Length > 0)
+            {
+                action = SubmitAction.Edit;
+                record = mod.getFile_By_FileUuid(file_uuid).AllRecord().First();
+            }
+            else
+            {
+                action = SubmitAction.Create;
+                record.FILE_UUID = LK.Util.UID.Instance.GetUniqueID();
+                record.FILE_CR = DateTime.Now;
+
+            }
+            record.FILE_NAME = file_name;
+            //record.FILE_URL = file_url;
+            record.FILE_PS = file_ps;
+            //record.FILE_PATH = file_path;
+            record.FILEGROUP_UUID = filegroup_uuid;
+            if (action == SubmitAction.Edit)
+            {
+                record.gotoTable().Update_Empty2Null(record);
+            }
+            else if (action == SubmitAction.Create)
+            {
+                record.gotoTable().Insert(record);
+                file_uuid = record.FILE_UUID;
+            }
+            System.Collections.Hashtable otherParam = new System.Collections.Hashtable();
+            otherParam.Add("FILE_UUID", record.FILE_UUID);
+            return ExtDirect.Direct.Helper.Message.Success.OutputJObject(otherParam);
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex); LK.MyException.MyException.Error(this, ex);
+            return ExtDirect.Direct.Helper.Message.Fail.OutputJObject(ex);
+        }
+    }
+
+    [DirectMethod("destoryFile", DirectAction.Store, MethodVisibility.Visible)]
+    public JObject destoryFile(string pFileUuid, Request request)
+    {
+        #region Declare
+        LwModel mod = new LwModel();
+        #endregion
+        try
+        {     /*Cloud身份檢查*/
+            checkUser(request.HttpRequest);
+            if (this.getUser() == null)
+            {
+                throw new Exception("Identity authentication failed.");
+            }/*權限檢查*/
+            if (!checkProxy(new StackTrace().GetFrame(0)))
+            {
+                throw new Exception("Permission Denied!");
+            };
+            /*取得總資料數*/
+
+            var data = mod.getFile_By_FileUuid(pFileUuid);
+            var runDel = false;
+            if (data.AllRecord().Count > 0)
+            {
+                var delDr = data.AllRecord().First();
+                delDr.gotoTable().Delete(delDr);
+                runDel = true;
+            }
+            if (runDel) {
+                var drFilegroup = mod.getFilegroup_By_FilegroupUuid(data.AllRecord().First().FILEGROUP_UUID).AllRecord().First();
+                var allFile = mod.getFile_By_FilegroupUuid(drFilegroup.FILEGROUP_UUID);
+
+                var displayName = "";
+                short fileCount = 0;
+                foreach (var item in allFile)
+                {
+                    displayName += item.FILE_NAME + ",";
+                    fileCount++;
+                }
+                drFilegroup.FILEGROUP_DISPLAY_NAME = displayName;
+                drFilegroup.FILE_COUNT = fileCount;
+                drFilegroup.gotoTable().Update_Empty2Null(drFilegroup);
+
+                
+            }
+            return ExtDirect.Direct.Helper.Message.Success.OutputJObject();
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex); LK.MyException.MyException.Error(this, ex);
+            /*將Exception轉成EXT Exception JSON格式*/
+            return ExtDirect.Direct.Helper.Message.Fail.OutputJObject(ex);
+        }
+    }
+
+    
+}
+
---
+++ Limew-web/direct/ShippingStatusAction.cs
@@ -0,0 +1,78 @@
+﻿#region USING
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using ExtDirect;
+using ExtDirect.Direct;
+using LK.DB.SQLCreater;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+using Limew.Model.Basic;
+using Limew.Model.Basic.Table;
+using Limew.Model.Basic.Table.Record;
+using Limew;
+using Limew.Model.Lw;
+using Limew.Model.Lw.Table;
+using Limew.Model.Lw.Table.Record;
+using System.Text;
+using LK.Util;
+using System.Data;
+using System.Diagnostics;
+#endregion
+
+[DirectService("ShippingStatusAction")]
+public class ShippingStatusAction : BaseAction
+{
+
+
+    [DirectMethod("loadShippingStatus", DirectAction.Store, MethodVisibility.Visible)]
+    public JObject loadShippingStatus(string pKeyword, string pageNo, string limitNo, string sort, string dir, Request request)
+    {
+        #region Declare
+        List<JObject> jobject = new List<JObject>();
+        Limew.Model.Basic.BasicModel model = new Limew.Model.Basic.BasicModel();
+        LwModel mod = new LwModel();
+        OrderLimit orderLimit = new OrderLimit();
+
+        #endregion
+        try
+        {     /*Cloud身份檢查*/
+            checkUser(request.HttpRequest);
+            if (this.getUser() == null)
+            {
+                throw new Exception("Identity authentication failed.");
+            }/*權限檢查*/
+            if (!checkProxy(new StackTrace().GetFrame(0)))
+            {
+                throw new Exception("Permission Denied!");
+            };
+            /*取得總資料數*/
+            orderLimit = ExtDirect.Direct.Helper.Order.getOrderLimit(pageNo, limitNo, sort, dir);
+            var totalCount = mod.getShippingStatus_By_Keyword_Count(pKeyword);
+            var data = mod.getShippingStatus_By_Keyword(pKeyword, orderLimit);
+            if (data.Count > 0)
+            {
+                jobject = JsonHelper.RecordBaseListJObject(data.ToList());
+            }
+            else
+            {
+                totalCount = 0;
+            }
+            /*使用Store Std out 『Sotre物件標準輸出格式』*/
+            return ExtDirect.Direct.Helper.Store.OutputJObject(jobject, totalCount);
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex); LK.MyException.MyException.Error(this, ex);
+            /*將Exception轉成EXT Exception JSON格式*/
+            return ExtDirect.Direct.Helper.Message.Fail.OutputJObject(ex);
+        }
+    }
+
+    
+}
+
